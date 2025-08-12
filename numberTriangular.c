261d534bfb59c050181e7e9edd4b9de264bca206
Ruta Jogani <ruta007jogani@gmail.com>
2025-08-12 20:05:08 +0530

    add: more lines in c code

---
+++ numberTriangular.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            if ((i + j) <= 5)
+            {
+                printf(" ");
+            }else
+            {
+                printf("%d", j);
+            }
+            
+        }
+        printf("\n");
+    }
+} 
