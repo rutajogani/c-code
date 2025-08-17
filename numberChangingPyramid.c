5f2c1ad69f2fbf9453c169088087cb13e0526e61
Ruta Jogani <ruta007jogani@gmail.com>
2025-08-17 15:34:40 +0530

    add: more lines in c code

---
+++ numberChangingPyramid.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int num = 0;
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            num++;
+            printf("%d", num);
+        }
+        printf("\n");
+    }
+}
