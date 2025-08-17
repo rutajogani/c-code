f889156b84e0af3f6681b5c4e4bdaf3a99e2527d
Ruta Jogani <ruta007jogani@gmail.com>
2025-08-17 21:24:27 +0530

    add: more lines in c code

--- reverseLeftHalfPyramid.c
+++ reverseLeftHalfPyramid.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 int main()
 {
-    for (int i = 1; i <= 5; i++)
+    for (int i = 5; i >= 1; i--)
     {
-        for (int j = 5; j >= 1; j--)
+        for (int j = 1; j <= i; j++)
         {
-            printf("*");
+            if (j >= i)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         printf("\n");
     }
+    return 0;
 }
--- rightPascalsTriangle.c
+++ rightPascalsTriangle.c
@@ -1,5 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+    int num = 0;
+    for (int i = 1; i <= 7; i++)
+    {
+        for (int j = i; j <= 4; j++)
+        {
+            num++;
+
+            if ((i + j) % 2 == 0)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
 }
