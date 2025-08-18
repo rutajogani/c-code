17e79d4cf2db67f6281a5bbe5707dd245bbb82ec
Ruta Jogani <ruta007jogani@gmail.com>
2025-08-18 21:22:36 +0530

    make and add lines

---
+++ new.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("Enter Number: ");
+    scanf("%d", &number);
+
+    for (int  i = 1; i <= number; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if ((i+j) <= number)
+            {
+                printf(" ");
+            }else
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
