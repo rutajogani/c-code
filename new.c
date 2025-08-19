e4d84ddf2fcedf345404beecec3150e9e8eaf6db
Ruta Jogani <ruta007jogani@gmail.com>
2025-08-19 21:39:26 +0530

    ad: lines in c

--- new.c
+++ new.c
@@ -1,7 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+    int number;
+    printf("Enter Number: ");
+    scanf("%d", &number);
 
+    for (int i = 1; i <= number; i++)
+    {
+        for (int j = 1; j <= number + 10; j++)
+        {
+            if (j % 2 == 0)
+            {
+                printf(" ");
+            }else
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+        
+    }
+    
+    
     return 0;
 }
